e7eaf080078ccf27e1841bd836941653de2a0027
Matt Wozniak <wozniamj01@gmail.com>
2023-07-10 07:03:19 -0400

    fixing junit.  adding test/resources. adding jsons

--- src/test/java/com/wozniak/game/TemporalNexus/TemporalNexusGameTest.java
+++ src/test/java/com/wozniak/game/TemporalNexus/TemporalNexusGameTest.java
@@ -1,65 +1,67 @@
 package com.wozniak.game.TemporalNexus;
 
 import com.wozniak.game.TemporalNexus.models.events.EventScenario;
+import com.wozniak.game.TemporalNexus.utils.EventUtils;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 public class TemporalNexusGameTest {
 
-    private String testFolderPath;
-    private List<EventScenario> eventScenarios;
-
     @BeforeEach
     public void setup() throws IOException {
-        // Create a temporary test folder and populate it with sample JSON files
-        testFolderPath = Files.createTempDirectory("TemporalNexusTest").toString();
-        createSampleJsonFile("event1.json", "{\"scene\":{\"id\":\"scene1\",\"description\":\"You are in a dark room.\"},\"choices\":[]}");
-        createSampleJsonFile("event2.json", "{\"scene\":{\"id\":\"scene2\",\"description\":\"You find yourself in a mysterious forest.\"},\"choices\":[]}");
-        eventScenarios = new ArrayList<>();
-    }
 
-    private void createSampleJsonFile(String fileName, String jsonContent) throws IOException {
-        Path filePath = Path.of(testFolderPath, fileName);
-        Files.writeString(filePath, jsonContent, StandardOpenOption.CREATE);
     }
 
     @Test
     public void testTemporalNexusGame() {
-        // Prepare the config.properties file with the test folder path
-        String configFilePath = Path.of(testFolderPath, "config.properties").toString();
+        String configFile = "C:\\Users\\Exagg\\coding\\git projects\\TemporalNexus\\TemporalNexus\\src\\test\\config.properties";
         Properties configProperties = new Properties();
-        configProperties.setProperty("json.folderPath", testFolderPath);
-        try {
-            configProperties.store(Files.newOutputStream(Path.of(configFilePath)), "Unit test config.properties");
+
+        try (FileInputStream fileInputStream = new FileInputStream(configFile)) {
+            configProperties.load(fileInputStream);
         } catch (IOException e) {
-            Assertions.fail("Failed to create config.properties file: " + e.getMessage());
+            System.out.println("Error loading config.properties file: " + e.getMessage());
+            return;
+        }
+
+        String folderPath = configProperties.getProperty("json.folderPath");
+        List<EventScenario> eventScenarios = new ArrayList<>();
+
+        File folder = new File(folderPath);
+        if (folder.isDirectory()) {
+            File[] files = folder.listFiles((dir, name) -> name.endsWith(".json"));
+            if (files != null) {
+                for (File file : files) {
+                    try {
+                        EventScenario eventScenario = EventUtils.createEventScenarioFromJson(file.getAbsolutePath());
+                        eventScenarios.add(eventScenario);
+                    } catch (IOException e) {
+                        System.out.println("Error reading JSON file: " + file.getName());
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
 
         // Invoke the main method of TemporalNexusGame
         TemporalNexusGame.main(new String[]{});
 
         // Assert that the expected number of EventScenario objects were created
-        int expectedEventScenarioCount = 2;
+        int expectedEventScenarioCount = 1;
         int actualEventScenarioCount = eventScenarios.size();
         Assertions.assertEquals(expectedEventScenarioCount, actualEventScenarioCount,
                 "Number of EventScenario objects created should match the expected count");
 
         // Assert the details of the EventScenario objects if needed
-        Assertions.assertEquals("You are in a dark room.", eventScenarios.get(0).getCurrentScene().getDescription());
-        Assertions.assertEquals("You find yourself in a mysterious forest.", eventScenarios.get(1).getCurrentScene().getDescription());
-    }
-
-    // Helper method to add EventScenario objects to the list
-    private void addEventScenario(EventScenario eventScenario) {
-        eventScenarios.add(eventScenario);
+        Assertions.assertEquals("Assassination of Julius Caesar", eventScenarios.get(0).getCurrentScene().getDescription());
     }
 }
